fcf1f7be57db9eefb8643a768d2e134ecc86c3bd
JeffreyMinucci <jminucci2@gmail.com>
2019-06-11 14:23:45 -0400

    add expocast and toxcast minimal dbs and finalize query

--- dsstox_rest/batch_query.py
+++ dsstox_rest/batch_query.py
@@ -13,7 +13,10 @@ parser.add_argument('search_by')
 parser.add_argument('query', action='append')
 parser.add_argument('accuracy', type=float)
 
-DSSTOX_PATH = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'database/dss_tox.db'))
+DSSTOX_PATH = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'database/dss_tox_3.db'))
+EXPOCAST_PATH = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'database/expocast.db'))
+ASSAY_PATH = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'database/assay_count.db'))
+
 
 class DsstoxBatchSearch(Resource):
 
@@ -42,12 +45,15 @@ class Dsstox_DB:
         self.c = None
         logging.info("=========== Dsstox DB connection established ===========")
 
-
-    def mass_search(self, query, accuracy):
+    def mass_search(self, query, accuracy):   # Note - accuracy is ppm
         self.c = self.conn.cursor()
+        self.c.execute('ATTACH DATABASE ? AS expocast;', (EXPOCAST_PATH,))
+        self.c.execute('ATTACH DATABASE ? AS assay;', (ASSAY_PATH,))
         db_results = pd.DataFrame(columns = ['INPUT', 'DTXCID_INDIVIDUAL_COMPONENT', 'MONOISOTOPIC_MASS_INDIVIDUAL_COMPONENT',
                                          'SMILES_INDIVIDUAL_COMPONENT', 'DTXSID', 'PREFERRED_NAME', 'CASRN',
-                                         'INCHIKEY', 'IUPAC_NAME', 'MOLECULAR_FORMULA'])
+                                         'INCHIKEY', 'IUPAC_NAME', 'MOLECULAR_FORMULA', 'MONOISOTOPIC_MASS',
+                                             'EXPOCAST_MEDIAN_EXPOSURE_PREDICTION_MG/KG-BW/DAY', 'EXPOCAST', 'NHANES',
+                                             'DATA_SOURCES', 'TOXCAST_PERCENT_ACTIVE','TOXCAST_NUMBER_OF_ASSAYS/TOTAL'])
         logging.info("=========== Searching Dsstox DB ===========")
         for i, mass in enumerate(query):
             #cursor2 = self.c.execute('''
@@ -58,13 +64,36 @@ class Dsstox_DB:
             #        AND gsc.fk_compound_id = c.id
             #        AND gsc.fk_generic_substance_id = gs.id;
             #        ''', (mass, accuracy))
+            mass = float(mass)
             cursor = self.c.execute('''
-                                SELECT c.dsstox_compound_id, c.monoisotopic_mass, c.smiles, gs.dsstox_substance_id,
-                                gs.preferred_name, gs.casrn, c.jchem_inchi_key, c.acd_iupac_name, c.mol_formula
-                                FROM generic_substance_compounds as gsc
-                                JOIN compounds as c ON c.id = gsc.fk_compound_id
+                                SELECT c_suc.dsstox_compound_id, c_suc.monoisotopic_mass, c_suc.smiles, gs.dsstox_substance_id,
+                                gs.preferred_name, gs.casrn, c.jchem_inchi_key, c.acd_iupac_name, c.mol_formula,
+                                c.monoisotopic_mass, em.Total_median, 
+                                CASE em.Total_median 
+                                    WHEN ''
+                                        THEN ''
+                                    ELSE "https://comptox.epa.gov/dashboard/dsstoxdb/results?search=" || gs.dsstox_substance_id || "#exposure-predictions"
+                                END EXPOCAST,
+                                CASE em.inNHANES
+                                    WHEN 'TRUE'
+                                        THEN "https://comptox.epa.gov/dashboard/dsstoxdb/results?search=" || gs.dsstox_substance_id || "#monitoring-data"
+                                      ELSE ''
+                                END NHANES,
+                                count(DISTINCT ss.fk_chemical_list_id)+1 as DATA_SOURCES,
+                                round(CAST(cac.assay_count_active AS FLOAT)/cac.assay_count_total*100,2) as TOXCAST_PERCENT_ACTIVE,
+                                cac.assay_count_active || "/" || cac.assay_count_total
+                                FROM compounds as c
+                                JOIN generic_substance_compounds as gsc ON c.id = gsc.fk_compound_id
                                 JOIN generic_substances as gs ON gsc.fk_generic_substance_id = gs.id
-                                WHERE abs(c.monoisotopic_mass - ?) < ?;
+                                JOIN compound_relationships as cr ON cr.fk_compound_id_predecessor = c.id
+                                JOIN compounds as c_suc ON c_suc.id = cr.fk_compound_id_successor
+                                JOIN source_generic_substance_mappings as sgsm ON sgsm.fk_generic_substance_id = gs.id
+                                JOIN source_substances as ss ON ss.id = sgsm.fk_source_substance_id
+                                JOIN assay.chemical_assay_count as cac ON cac.chid = gs.id
+                                JOIN expocast.expocast_models as em ON em.casrn = gs.casrn 
+                                WHERE abs(c_suc.monoisotopic_mass - ?) < ? AND cr.fk_compound_relationship_type_id == 2
+                                AND c.has_defined_isotope == 0
+                                ORDER BY DATA_SOURCES DESC;
                                 ''', (mass, accuracy))
             logging.info("=========== Parsing results ===========")
             for row in cursor:
@@ -75,7 +104,7 @@ class Dsstox_DB:
         #db_results['INPUT'] = query
         db_results['MASS_DIFFERENCE'] = abs(db_results['INPUT'].astype(float) -
                                                   db_results['MONOISOTOPIC_MASS_INDIVIDUAL_COMPONENT'].astype(float))
-        db_results['FOUND_BY'] = 'Mass'
+        db_results['FOUND_BY'] = 'Monoisotopic Mass'
         results_db_dict = db_results.to_dict(orient='list')
         return jsonify(results_db_dict)
 
